0f62f797c7590bdd2340d54db132f60c3cebb6d7
Angelo Battaglia <angelobattaglia.math@gmail.com>
2024-09-11 22:20:01 +0200

    Commenting a result

--- Temi/NonLinear_Equations/esame_2_4_2022_es_3.m
+++ Temi/NonLinear_Equations/esame_2_4_2022_es_3.m
@@ -20,7 +20,7 @@ for i = 1:n
     xn = g(xn);
 end
 
-relative_error = abs(xn - x0) / abs(x0)
+% relative_error = abs(xn - x0) / abs(x0)
 
 % The "exact" value of the fixed point
 exact_value = -9.207028302184803e-01;
